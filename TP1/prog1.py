e8703c7acf1d12e5bb5c17da6f26e69cd1792100
tp-r504 <paul.nqtt@gmail.com>
2024-09-16 15:27:17 +0200

    Ajout de l'affichage du nombre carré

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -2,3 +2,21 @@ print("taper un nombre")
 a=int(input())
 print("a+1",a+1)
 
+while True:
+    try:
+        # Saisir un nombre au clavier
+        nombre = float(input("Entrez un nombre: "))
+        
+        # Afficher le carré du nombre
+        print(f"Le carré de {nombre} est {nombre ** 2}")
+    
+    except ValueError:
+        # Gérer le cas où l'utilisateur entre autre chose qu'un nombre
+        print("Veuillez entrer un nombre valide.")
+    
+    except KeyboardInterrupt:
+        # Sortir de la boucle infinie avec CTRL-C
+        print("\nBoucle interrompue. Au revoir!")
+        break
+
+
