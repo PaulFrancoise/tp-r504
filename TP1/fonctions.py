5f6a091c4ffcafcd84ddc652e3454c24c64b7157
tp-r504 <paul.nqtt@gmail.com>
2024-09-16 15:31:43 +0200

    Ajout d'un nombre à sa puissance

---
+++ TP1/fonctions.py
@@ -0,0 +1,7 @@
+# Test de la fonction
+a = int(input("Entrez la base (a): "))
+b = int(input("Entrez l'exposant (b): "))
+resultat = (a**b)
+
+print(f"{a} élevé à la puissance {b} est {resultat}")
+
